939b8861aaa07d7e61dcd4d4a25979e54a2a306d
Sam Oaks <samuelclayoaks@yahoo.com>
2023-03-07 16:55:35 -0500

    added starter js code, including api key

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+var APIkey = "e8c6163569b8625c5a5cab8bf461be7e";
+
+var cityInputEl = $('#city-input');
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear-button');
+var pastSearchedCitiesEl = $('#past-searches');
+
+var currentCity;
+
+function getWeather() {
+
+}
