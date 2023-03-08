15bcbb71fef86dccd4893f30bf6027eb7cb03991
Sam Oaks <samuelclayoaks@yahoo.com>
2023-03-08 16:24:14 -0500

    added function to display search history

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,23 @@ var pastSearchedCitiesEl = $('#past-searches');
 
 var currentCity;
 
+function displaySearchHistory() {
+    var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+    var pastSearchesEl = document.getElementById('past-searches');
+
+    pastSearchesEl.innerHTML ='';
+
+    for (i = 0; i < storedCities.length; i++) {
+        
+        var pastCityBtn = document.createElement("button");
+        pastCityBtn.classList.add("btn", "btn-primary", "my-2", "past-city");
+        pastCityBtn.setAttribute("style", "width: 100%");
+        pastCityBtn.textContent = `${storedCities[i].city}`;
+        pastSearchesEl.appendChild(pastCityBtn);
+    }
+    return;
+}
+
 function getWeather() {
     var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
     fetch(requestUrl)
